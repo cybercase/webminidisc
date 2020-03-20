d84e6a4e72a401427e5cd4e45d4c0825188d0f30
Stefano Brilli <stefanobrilli@gmail.com>
2020-03-20 18:45:53 +0100

    Updates

---
+++ precache-manifest.7fc19003429530d06070b30e4bcc08ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d051fad4511303ffac452d3fbc365597",
+    "url": "/webminidisc/036c27ad182506b1360e.worker.js"
+  },
+  {
+    "revision": "2db20f781fcda8eaf16b6c6a656de7bf",
+    "url": "/webminidisc/036c27ad182506b1360e.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "de655ff8199b20b4db07ce1dfc20bf06",
+    "url": "/webminidisc/ce95054843396120d61b.worker.js"
+  },
+  {
+    "revision": "6594e123dc3068915103e9623600a2b6",
+    "url": "/webminidisc/index.html"
+  },
+  {
+    "revision": "7cd376891b9c68a4d245",
+    "url": "/webminidisc/static/css/main.0b990f46.chunk.css"
+  },
+  {
+    "revision": "de6a882f5237f8801a45",
+    "url": "/webminidisc/static/js/2.126c9f2d.chunk.js"
+  },
+  {
+    "revision": "2e010bb9b670fcf3c9123c0544f4b757",
+    "url": "/webminidisc/static/js/2.126c9f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd376891b9c68a4d245",
+    "url": "/webminidisc/static/js/main.a921304d.chunk.js"
+  },
+  {
+    "revision": "b873df5f5158f06b4f7b",
+    "url": "/webminidisc/static/js/runtime-main.114d47b9.js"
+  },
+  {
+    "revision": "f3b6c54c5cc90485a549a1ae6699a9c4",
+    "url": "/webminidisc/static/media/chrome-icon.f3b6c54c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3abddb5d2fcc04f6350ebb0f7de17eb.js"
+  "/webminidisc/precache-manifest.7fc19003429530d06070b30e4bcc08ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webminidisc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
