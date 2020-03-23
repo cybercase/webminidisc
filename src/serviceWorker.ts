4dc41ed89c1fe21073b8fba8c850d4fe7d3d6a44
Stefano Brilli <stefanobrilli@gmail.com>
2020-03-23 23:45:09 +0100

    Cache public required files for offline usage

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -35,7 +35,7 @@ export function register(config?: Config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            const swUrl = `${process.env.PUBLIC_URL}/custom-service-worker.js`;
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
