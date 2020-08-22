6d042ef43b11308ac6f89e3cc1387c126b745679
Stefano Brilli <stefanobrilli@gmail.com>
2020-08-22 11:52:03 +0200

    Updates

--- extra-service-worker.js
+++ extra-service-worker.js
@@ -1,6 +1,6 @@
 // List of extra files to be precached for offline use.
 // WARNING: don't forget to update the revision before deploy
-var revision = "8";
+var revision = "9";
 self.__precacheManifest = (self.__precacheManifest || []).concat([
     {
         "revision": revision,
