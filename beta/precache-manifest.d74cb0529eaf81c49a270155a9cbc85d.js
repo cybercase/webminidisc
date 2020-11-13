64050d15901ee63f80d6ec1c1e071fb14928579f
Stefano Brilli <stefanobrilli@gmail.com>
2020-11-14 00:14:14 +0100

    Updates

---
+++ beta/precache-manifest.d74cb0529eaf81c49a270155a9cbc85d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153d46e20c1b06cdbaeedda864be3c75",
+    "url": "/webminidisc/beta/0aba6782ae4344385c04.worker.js"
+  },
+  {
+    "revision": "404d7630f6ada4f68aba85ddc3181775",
+    "url": "/webminidisc/beta/0aba6782ae4344385c04.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "90caf112c7aee3c283fb46780f5f9044",
+    "url": "/webminidisc/beta/7a2cdea617fc7c50ff57.worker.js"
+  },
+  {
+    "revision": "93fdc2e85826c7ba2487cb1e609bdf6d",
+    "url": "/webminidisc/beta/7a2cdea617fc7c50ff57.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab7567fb1386531a28d00a1db6a3e9a",
+    "url": "/webminidisc/beta/index.html"
+  },
+  {
+    "revision": "24dc1468605761fb0056",
+    "url": "/webminidisc/beta/static/css/main.6f13b01b.chunk.css"
+  },
+  {
+    "revision": "79c68752a3c38307e3a1",
+    "url": "/webminidisc/beta/static/js/2.e5665fb3.chunk.js"
+  },
+  {
+    "revision": "9ff654d7e762e0d8656f1e7eb5023f9c",
+    "url": "/webminidisc/beta/static/js/2.e5665fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dc1468605761fb0056",
+    "url": "/webminidisc/beta/static/js/main.574941a6.chunk.js"
+  },
+  {
+    "revision": "c1746835fe08500d7958",
+    "url": "/webminidisc/beta/static/js/runtime-main.792784c4.js"
+  },
+  {
+    "revision": "1cb04ca62aac474ee9ab86b20b54a6a2",
+    "url": "/webminidisc/beta/static/media/LCDDot-Regular.1cb04ca6.woff2"
+  },
+  {
+    "revision": "5bca0e6226c5efcdf97dcfb4793b9037",
+    "url": "/webminidisc/beta/static/media/LCDDot-Regular.5bca0e62.ttf"
+  },
+  {
+    "revision": "70bbb7a203288d5e26e87802988d6d36",
+    "url": "/webminidisc/beta/static/media/LCDDot-Regular.70bbb7a2.svg"
+  },
+  {
+    "revision": "7228d5e94e4339e18e1da2392b5fbb7b",
+    "url": "/webminidisc/beta/static/media/LCDDot-Regular.7228d5e9.woff"
+  },
+  {
+    "revision": "f9fc1d8a4423bd1bbc387e0de30102f7",
+    "url": "/webminidisc/beta/static/media/LCDDot-Regular.f9fc1d8a.eot"
+  },
+  {
+    "revision": "f3b6c54c5cc90485a549a1ae6699a9c4",
+    "url": "/webminidisc/beta/static/media/chrome-icon.f3b6c54c.svg"
+  },
+  {
+    "revision": "a7574f835e639c7eccf1884c27c2939d",
+    "url": "/webminidisc/beta/static/media/md0.a7574f83.svg"
+  },
+  {
+    "revision": "1ffb5f6c6a4046d967788635cd5e8d12",
+    "url": "/webminidisc/beta/static/media/md1.1ffb5f6c.svg"
+  },
+  {
+    "revision": "33dd03b3ad6bc789f33d63700db75862",
+    "url": "/webminidisc/beta/static/media/md2.33dd03b3.svg"
+  },
+  {
+    "revision": "80f42826469e90375bd01f3e5b3e5458",
+    "url": "/webminidisc/beta/static/media/md3.80f42826.svg"
+  }
+]);
