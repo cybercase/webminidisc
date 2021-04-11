439af495ca5ee74b9ccc7ec2ab250e55b18b1f0e
Stefano Brilli <stefanobrilli@gmail.com>
2021-04-11 13:05:03 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webminidisc/precache-manifest.21d64baad94545e27890258c5b80c76a.js"
+  "/webminidisc/precache-manifest.5a1c2a7999a176ac3492919fbb282b94.js"
 );
 
 self.addEventListener('message', (event) => {
